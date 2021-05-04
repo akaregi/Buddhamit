d7af3a9d34222ab5ef09311618e5e88d5ab5b3da
akaregi <verdigris@azure.icu>
2021-05-04 13:06:46 +0900

    Added aliases for sample
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/Sample.ts
+++ src/Sample.ts
@@ -3,6 +3,8 @@ import { Command, Message } from "discord.js"
 const command: Command = {
     name: '',
     description: '',
+    aliases: [''],
+
     execute(ctx: Message, args: string[]) {
         ctx.react('👍')
     }
