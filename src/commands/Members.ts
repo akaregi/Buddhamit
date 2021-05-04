342b6e1bb238073bd27b042adbdfdbf8db8118e3
akaregi <verdigris@azure.icu>
2021-05-04 11:20:17 +0900

    Fixed in DM channel
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/commands/Members.ts
+++ src/commands/Members.ts
@@ -4,6 +4,11 @@ const command: Command = {
     name: 'members',
     description: '氏子の人数を出します。',
     execute (ctx: Message, args: string[]) {
+        if (ctx.channel.type === 'dm') {
+            ctx.reply('氏子はあなたしかいません。')
+            return
+        }
+
         ctx.react('👍')
         ctx.reply(`氏子は${ctx.guild?.memberCount}人います。`)
     }
