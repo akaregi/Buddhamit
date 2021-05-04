591198c6ed42695409628aecb2c6229ec82e4180
akaregi <verdigris@azure.icu>
2021-05-04 23:07:29 +0900

    Implemented disconnect
    Signed-off-by: akaregi <verdigris@azure.icu>

---
+++ src/commands/Disconnect.ts
@@ -0,0 +1,23 @@
+import { Command, Message } from 'discord.js'
+
+const command: Command = {
+    name: 'disconnect',
+    description: 'VCから強制切断させます。',
+
+    // NOTE: args must be implemented by discord.d.ts
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async execute(ctx: Message, args: string[]) {
+        ctx.react('👍')
+
+        const channel = ctx.member?.voice.channel
+
+        if (channel) {
+            channel.leave()
+            return
+        }
+
+        ctx.reply('ブッダは接続していない。')
+    }
+}
+
+export = command
