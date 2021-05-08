01345f5b640aef2b0b7edabce79da9605d94fbf9
akaregi <verdigris@azure.icu>
2021-05-08 15:31:51 +0900

    Improved -mp
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/commands/Manpower.ts
+++ src/commands/Manpower.ts
@@ -17,24 +17,67 @@ const command: Command = {
             return die(ctx, 'ロールを指定してください、舎利子よ。')
         }
 
-        if (!args[0].startsWith('<@&') || !args[0].endsWith('>')) {
-            return die(ctx, 'ロール指定がアラーです、舎利子。')
+        const role = ctx.guild?.roles.cache
+            .find(role => {
+                const name = role.name.toLowerCase()
+                const object = args[0]?.toLowerCase()
+
+                return !name.indexOf(object ?? '')
+            })
+
+        if (!role) {
+            ctx.client.logger.debug(args[0])
+            return die(ctx, '舎利子よ、そのロールは存在しない。')
         }
 
-        const role = args[0]?.slice(3).slice(0, -1)
-
-        const members = (await fetchUserAll(ctx))
+        const roleMembers = (await fetchUserAll(ctx))
             ?.array()
-            .filter(member => member.roles.cache.has(role))
+            .filter(member => member.roles.cache.has(role.id))
+
+        const members = roleMembers
+            ?.filter(member => !member.voice.channel)
 
         if (!members) {
-            return die(ctx, 'そのロールに属する舎利子は存在しない。')
+            return die(ctx, '徴兵できる舎利子は存在しない。')
         }
 
         const candicate = members[Math.floor(Math.random() * members.length)]
 
+        if (!candicate) {
+            return die(ctx, '徴兵できる舎利子は存在しない。')
+        }
+
         ctx.react('👍')
-        ctx.channel.send(`${candicate} が徴兵対象です。`)
+        ctx.channel.send(
+            `${(new Date()).getMonth() + 1}月度勤労抽選により「${candicate}」は「**${role.name}**」のために徴兵される。`
+        )
+
+        if (args[1] && args[1] === '-v') {
+            // Role members
+            const targetMembers = roleMembers
+                ?.map(member => member.user.username)
+                .join(', ')
+
+            // Excluded due to VC connection
+            const excludeMembers = roleMembers
+                ?.filter(member => member.voice.channel !== null)
+                .map(member => member.user.username)
+                .join(', ')
+
+            // Final candicates
+            const includeMembers = members
+                .map(member => member.user.username)
+                .join(', ')
+
+            ctx.channel.send(
+                'デバッグ情報: \n' +
+                `対象ロール \`@${role.name} (${role.id})\` \n` +
+                `ロール持ち: ${targetMembers} \n` +
+                `VC により除外: ${excludeMembers ?? 'なし'} \n` +
+                `**候補**: ${includeMembers}`
+            )
+
+        }
     }
 }
 
