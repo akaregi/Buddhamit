cc8b8c57a6cd81983d4dc17f95c70c8f2e2d0d92
akaregi <verdigris@azure.icu>
2021-05-08 18:02:44 +0900

    Added option username
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/commands/Value.ts
+++ src/commands/Value.ts
@@ -1,13 +1,16 @@
-import { Command, Message } from "discord.js"
+import { Command, Message } from 'discord.js'
 
 const command: Command = {
     name: 'value',
     description: 'お前の価値を算出します。',
+    usage: 'value [name]',
     aliases: ['俺の価値'],
 
     execute(ctx: Message, args: string[]) {
+        const name = args[0] ? `「${args[0]}」` : 'お前'
+
         ctx.react('👍')
-        ctx.reply(`お前の価値は${Math.ceil(Math.random() * 100)}/100だ。`)
+        ctx.reply(`${name}の価値は「**${Math.ceil(Math.random() * 100)}/100**」だ。`)
     }
 }
 
